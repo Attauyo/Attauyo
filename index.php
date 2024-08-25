25c863bc5637125e7b8f01c486fbc3b0cfcadb0e
Attauyo <136933744+Attauyo@users.noreply.github.com>
2024-08-25 21:52:37 +0100

    Update and rename admin_dashboard.php to index.php

--- index.php
+++ index.php
@@ -1,33 +1,23 @@
-<?php
-include('../db_connection.php');
-
-// Assuming admin is logged in and this page displays after login
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
+    <title>Bitcoin Broker Platform</title>
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
-    <link href="../css/custom_styles.css" rel="stylesheet">
+    <link href="css/custom_styles.css" rel="stylesheet">
 </head>
 <body>
     <div class="container">
-        <h2 class="text-center mt-5">Admin Dashboard</h2>
+        <h1 class="text-center mt-5">Welcome to the Bitcoin Broker Platform</h1>
         <div class="row mt-4">
-            <div class="col-md-4">
-                <a href="manage_investments.php" class="btn btn-primary btn-block">Manage Investments</a>
+            <div class="col-md-6">
+                <a href="user/user_login.php" class="btn btn-primary btn-block">User Login</a>
             </div>
-            <div class="col-md-4">
-                <a href="manage_notifications.php" class="btn btn-warning btn-block">Manage Notifications</a>
-            </div>
-            <div class="col-md-4">
-                <a href="logout.php" class="btn btn-danger btn-block">Logout</a>
+            <div class="col-md-6">
+                <a href="admin/admin_dashboard.php" class="btn btn-secondary btn-block">Admin Login</a>
             </div>
         </div>
     </div>
@@ -37,6 +27,6 @@ include('../db_connection.php');
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <!-- Custom JS -->
-    <script src="../js/custom_scripts.js"></script>
+    <script src="js/custom_scripts.js"></script>
 </body>
 </html>
