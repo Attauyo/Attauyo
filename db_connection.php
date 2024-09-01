b20035f46a14fb6408051582e57d90e6321101a0
Attauyo <136933744+Attauyo@users.noreply.github.com>
2024-09-01 09:00:34 +0100

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "fdb1028.awardspace.net"; // Your database server address
-$username = "4521183_bitcoin";        // Your MySQL username
-$password = "09058176690Ol";          // Your MySQL password
-$dbname = "4521183_bitcoin";          // Your database name
+$servername = "sql300.ezyro.com"; // Your database server address
+$username = "ezyro_37220795";        // Your MySQL username
+$password = "09058176690";          // Your MySQL password
+$dbname = "ezyro_37220795_endless0147";          // Your database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
